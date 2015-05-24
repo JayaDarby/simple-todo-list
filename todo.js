fd38f8fefa2703a8113385585c914aa79dc0a99d
JayaDarby <jayadarby@gmail.com>
2015-05-24 13:45:22 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,14 +4,46 @@ document.querySelector('input').addEventListener('focus', function(event){
     event.preventDefault();
 });
 
+document.body.addEventListener('mouseover', function(event){
+	if(event.srcElement.nodeName == 'SPAN'){
+      event.srcElement.style.color = 'red';
+    }
+    event.preventDefault();
+});
+
+document.body.addEventListener('mouseout', function(event){
+	if(event.srcElement.nodeName == 'SPAN'){
+      event.srcElement.style.color = 'white';
+    }
+    event.preventDefault();
+});
+
 document.querySelector('form').addEventListener('submit', function(event){
-	event.preventDefault();
 	var item = document.createElement('LI');
 	var input = document.getElementById('todoItem');
     item.innerHTML = input.value;
-
-    document.querySelector('UL').appendChild(item);
+    event.preventDefault();
+    var deleteSpan = document.createElement('SPAN');
+    deleteSpan.style.float='right';
+    deleteSpan.style.color='white';
+    deleteSpan.innerHTML = 'Delete';
+    if(input.value != ''){
+        document.querySelector('UL').appendChild(item);
+        item.appendChild(deleteSpan);
+        document.querySelector('input').value='';
+    }
 });
 
+document.body.addEventListener('click', function(event){	
+    if(event.srcElement.nodeName == 'SPAN')	{
+        event.srcElement.parentElement.remove();
+    }
+    else if(event.srcElement.nodeName == 'LI'){
+		if(event.srcElement.className)
+			event.srcElement.className='';
+		else
+		  event.srcElement.className = 'completed';
+    }
+})
 
 
